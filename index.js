f63e052e7814541fc162bd88a813801d23094ec0
Koby Fink <finkkoby@icloud.com>
2024-02-08 09:14:35 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method:"POST",
+        headers: {
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+        },
+        body: JSON.stringify({
+            'name': `${name}`,
+            'email': `${email}`
+        })
+    })
+    .then(response => response.json())
+    .then(object => {
+        let objId = object.id;
+        document.body.innerHTML = `${objId}`;
+    })
+    .catch(function(error) {
+        document.body.innerHTML = `${error}`;
+    })
+}
+submitData("Koby", "email@email.com")
